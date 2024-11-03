ffd7cf84cb8d986ad2abe955de6c50f127389fe9
Roberto Silva <robertocunha.silva@gmail.com>
2024-11-03 19:16:39 -0300

    copilot

---
+++ Lista415.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Lista415 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int divisor = 0, numero, n = 1;
+        
+        System.out.println("Escreva um número:");
+        numero = in.nextInt();;
+
+        while (n <= numero){
+            if (numero % n == 0){
+                divisor ++;
+            }
+        n++;
+        }
+
+        if (divisor == 2){
+            System.out.println("é primo");
+        } else {
+            System.out.println("não é primo");
+        }
+
+        in.close();
+    }
+
+}
---
+++ Lista416.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Lista416 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        
+
+        in.close();
+    }
+
+}
